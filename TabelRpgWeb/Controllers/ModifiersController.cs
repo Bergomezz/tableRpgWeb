420b597d79330186e46a321867c9d4c584736e9c
Bergomezz <bernardogomezz@yahoo.com.br>
2024-04-01 13:53:48 -0300

    (refactoring) Change the modifier name to be in the good pratices

---
+++ TabelRpgWeb/Controllers/ModifiersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using TabelRpgWeb;
+
+namespace TabelRpgWeb
+{
+    public class Modifiers
+    {
+
+        public int CalculateModifier(int attribute)
+        {
+            int modifier = -5;
+
+            int chosenAttribute = modifier;
+            if (attribute == 0)
+            {
+                return chosenAttribute;
+            }
+            else
+            {
+                for (int i = 1; i <= attribute; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        chosenAttribute = modifier += 1;
+                    }
+                }
+                return chosenAttribute;
+            }
+        }
+    }
+}
